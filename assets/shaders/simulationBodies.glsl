2542c44af35f3424040c7fe74afc4adcc76054ed
Ronie Salgado <roniesalg@gmail.com>
2024-07-17 07:14:07 +0200

    Use tiles for accumulating the morse potential.

--- assets/shaders/simulationBodies.glsl
+++ assets/shaders/simulationBodies.glsl
@@ -68,6 +68,13 @@ const uint TileSize = 32u;
 shared vec3 tileAtomPositions[TileSize];
 shared vec2 tileAtomCoefficients[TileSize];
 
+shared uint tileBondFirstAtomIndex[TileSize];
+shared uint tileBondSecondAtomIndex[TileSize];
+shared vec3 tileBondFirstAtomPosition[TileSize];
+shared float tileBondMorseEquilibriumDistance[TileSize];
+shared float tileBondMorseWellDepth[TileSize];
+shared float tileBondMorseWellWidth[TileSize];
+
 void main()
 {
     uint myAtomIndex = gl_GlobalInvocationID.x;
@@ -116,30 +123,51 @@ void main()
     }
 
     // Bond morse potential.
-    for(uint bondIndex = 0u; bondIndex < bondCount; ++bondIndex)
+    for(uint bondTileIndex = 0u; bondTileIndex < bondCount; bondTileIndex += TileSize)
     {
-        if(AtomBondDescriptionBuffer[bondIndex].secondAtomIndex != myAtomIndex)
-            continue;
-
-        uint secondAtomIndex = myAtomIndex;
-        vec3 secondPosition = myAtomPosition;
-
-        uint firstAtomIndex = AtomBondDescriptionBuffer[bondIndex].firstAtomIndex;
-        vec3 firstPosition = AtomStateBuffer[firstAtomIndex].position;
-
-        float morseEquilibriumDistance = AtomBondDescriptionBuffer[bondIndex].morseEquilibriumDistance;
-        float morseWellDepth = AtomBondDescriptionBuffer[bondIndex].morseWellDepth;
-        float morseWellWidth = AtomBondDescriptionBuffer[bondIndex].morseWellWidth;
-
-        vec3 direction = secondPosition - firstPosition;
-        float dist = length(direction);
-        if(dist > 0.000001)
+        uint fetchTileElementIndex = gl_LocalInvocationID.x;
+        uint fetchBondIndex = bondTileIndex + fetchTileElementIndex;
+        if(fetchBondIndex < bondCount)
         {
-            direction /= dist;
+            tileBondFirstAtomIndex[fetchTileElementIndex] = AtomBondDescriptionBuffer[fetchBondIndex].firstAtomIndex;
+            tileBondSecondAtomIndex[fetchTileElementIndex] = AtomBondDescriptionBuffer[fetchBondIndex].secondAtomIndex;
 
-            vec3 force = -direction * morsePotentialDerivative(dist, morseWellDepth, morseWellWidth, morseEquilibriumDistance);
-            myNetForce += force;
+            tileBondFirstAtomPosition[fetchTileElementIndex] = AtomStateBuffer[tileBondFirstAtomIndex[fetchTileElementIndex]].position;
+            
+            tileBondMorseEquilibriumDistance[fetchTileElementIndex] = AtomBondDescriptionBuffer[fetchBondIndex].morseEquilibriumDistance;
+            tileBondMorseWellDepth[fetchTileElementIndex] = AtomBondDescriptionBuffer[fetchBondIndex].morseWellDepth;
+            tileBondMorseWellWidth[fetchTileElementIndex] = AtomBondDescriptionBuffer[fetchBondIndex].morseWellWidth;
         }
+        barrier();
+
+        for(uint bondTileElementIndex = 0u; bondTileElementIndex < TileSize; ++bondTileElementIndex)
+        {
+            uint bondIndex = bondTileIndex + bondTileElementIndex;
+            if(bondIndex >= bondCount || tileBondSecondAtomIndex[bondTileElementIndex] != myAtomIndex)
+                continue;
+
+            uint secondAtomIndex = myAtomIndex;
+            vec3 secondPosition = myAtomPosition;
+
+            uint firstAtomIndex = tileBondFirstAtomIndex[bondTileElementIndex];
+            vec3 firstPosition = tileBondFirstAtomPosition[bondTileElementIndex];
+
+            float morseEquilibriumDistance = tileBondMorseEquilibriumDistance[bondTileElementIndex];
+            float morseWellDepth = tileBondMorseWellDepth[bondTileElementIndex];
+            float morseWellWidth = tileBondMorseWellWidth[bondTileElementIndex];
+
+            vec3 direction = secondPosition - firstPosition;
+            float dist = length(direction);
+            if(dist > 0.000001)
+            {
+                direction /= dist;
+
+                vec3 force = -direction * morsePotentialDerivative(dist, morseWellDepth, morseWellWidth, morseEquilibriumDistance);
+                myNetForce += force;
+            }
+        }
+
+        barrier();
     }
 
     AtomStateBuffer[myAtomIndex].netForce = myNetForce;
