5f4b9e89fba57fe356eed1141b68a46da3c51145
Ronie Salgado <roniesalg@gmail.com>
2025-06-02 12:30:18 -0400

    Quick exponentiation.

--- apps/Potentials.hpp
+++ apps/Potentials.hpp
@@ -10,16 +10,26 @@ const double SimulationTimeStep = 1e-3f; // Picoseconds
 const double BoltzmannConstantSI = 1.380649e-23; // m^2.K^-1
 const double TargetTemperature = 10; // Kelvin
 
+__host__ __device__ inline double quickPow(double base, int exponent)
+{
+    if(exponent == 0)
+        return 1;
+    else if((exponent & 1) == 0)
+        return quickPow(base*base, exponent>>1);
+    else
+        return base * quickPow(base*base, exponent>>1);
+}
+
 inline double
 lennardJonesPotential(double r, double sigma, double epsilon)
 {
-    return 4*epsilon*(pow(sigma/r, 12) - pow(sigma/r, 6));
+    return 4*epsilon*(quickPow(sigma/r, 12) - quickPow(sigma/r, 6));
 }
 
 inline __host__ __device__ double
 lennardJonesDerivative(double r, double sigma, double epsilon)
 {
-    return 24*epsilon*(pow(sigma, 6)/pow(r, 7) - 2.0*pow(sigma, 12)/pow(r, 13));
+    return 24*epsilon*(quickPow(sigma, 6)/quickPow(r, 7) - 2.0*quickPow(sigma, 12)/quickPow(r, 13));
 }
 
 inline double
